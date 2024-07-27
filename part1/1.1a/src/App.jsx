79d6f6af883a21c958afec2e4a97ac4aa3c9da63
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-07-27 17:47:28 +0530

    exercises 1.1 and 1.2

---
+++ part1/1.1a/src/App.jsx
@@ -0,0 +1,55 @@
+const Header = (props) => {
+  return <h1>{props.course}</h1>
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+      <Part part={props.parts[0].part1} exercises={props.parts[0].exercises1} />
+      <Part part={props.parts[1].part2} exercises={props.parts[1].exercises2} />
+      <Part part={props.parts[2].part3} exercises={props.parts[2].exercises3} />
+    </>
+  )
+}
+
+const Total = (props) => {
+  return <p>Number of exercises {props.total}</p>
+}
+
+function App() {
+  const course = "Half Stack application development"
+  const parts = [
+    {
+      part1: "Fundamentals of React",
+      exercises1: 10,
+    },
+    {
+      part2: "Fundamentals of JS",
+      exercises2: 7,
+    },
+    {
+      part3: "Fundamentals of Next",
+      exercises3: 14,
+    },
+  ]
+
+  return (
+    <div>
+      <Header course={course}></Header>
+      <Content parts={parts} />
+      <Total
+        total={parts[0].exercises1 + parts[1].exercises2 + parts[2].exercises3}
+      />
+    </div>
+  )
+}
+
+export default App
