7c6143b1ccdcce8df8d8181a09a935fa2481eefe
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-08-03 15:23:39 +0530

    exercise 2.11 done

--- part2/practice/src/App.jsx
+++ part2/practice/src/App.jsx
@@ -1,10 +1,24 @@
-import { useState } from "react"
+import { useState,useEffect } from "react"
 import Note from "./Components/Note"
+import axios from 'axios'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState("")
   const [showAll, setShowAll] = useState(true)
+
+useEffect(() => {
+  console.log('effect');
+  axios
+  .get('http://localhost:3001/notes')
+  .then(response => {
+    console.log('promise fullfilled');
+    
+    setNotes(response.data)
+  })
+},[])
+console.log("render", notes.length,'notes');
+
 
   const addNote = (e) => {
     e.preventDefault()
