a907c4b00ff853c7997d494520704f843881c169
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-08-05 14:59:13 +0530

    exercises 2.18 finished

---
+++ part2/countries/src/App.jsx
@@ -0,0 +1,69 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+
+function App() {
+  const [countries, setCountries] = useState([])
+  const [filter, setfilter] = useState("")
+
+  useEffect(() => {
+    axios
+      .get("https://studies.cs.helsinki.fi/restcountries/api/all")
+      .then((res) => {
+        setCountries(res.data)
+      })
+  }, [countries])
+
+  const handleFilter = (e) => {
+    setfilter(e.target.value)
+  }
+  const filterCountries = countries.filter((country) =>
+    country.name.common.toLowerCase().includes(filter.toLowerCase())
+  )
+
+  return (
+    <div>
+      <div>
+        <label htmlFor="filterCountry">Filter countries</label>
+        <input
+          type="text"
+          id="filterCountry"
+          value={filter}
+          onChange={handleFilter}
+        />
+      </div>
+      <div>
+        {filterCountries.length > 10 ? (
+          <p>Too many matches, specify another filter</p>
+        ) : filterCountries.length === 1 ? (
+          <div>
+            <h1>{filterCountries[0].name.common}</h1>
+            <p>capital {filterCountries[0].capital}</p>
+            <p>area {filterCountries[0].area}</p>
+            <div>
+              <h2>Languages</h2>
+              <ul>
+                {Object.values(filterCountries[0].languages).map(
+                  (lang, index) => (
+                    <li key={index}>{lang}</li>
+                  )
+                )}
+              </ul>
+              <div>
+                <img
+                  src={filterCountries[0].flags.png}
+                  alt={filterCountries[0].flags.alt}
+                />
+              </div>
+            </div>
+          </div>
+        ) : (
+          filterCountries.map((country) => (
+            <p key={country.cca3}>{country.name.common}</p>
+          ))
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default App
