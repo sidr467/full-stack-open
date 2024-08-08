56c8b13a5aac7af9694e64f258711de99bfa6c3d
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-08-08 15:44:50 +0530

    exercises 3.15 to 3.18 done

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -7,48 +7,114 @@ app.use(express.json())
 app.use(cors())
 app.use(express.static("dist"))
 
-app.get("/api/persons", (req, res) => {
-  Person.find({}).then((people) => {
-    res.json(people)
+app.get("/api/persons", (req, res, next) => {
+  Person.find({})
+    .then((people) => {
+      res.json(people)
+    })
+    .catch((error) => {
+      next(error)
+    })
+})
+
+app.get("/api/persons/:id", (req, res, next) => {
+  const id = req.params.id
+  Person.findById(id)
+    .then((result) => {
+      if (result) {
+        res.json(result)
+      } else {
+        res.status(400).end()
+      }
+    })
+    .catch((error) => {
+      next(error)
+    })
+})
+
+app.get("/info", (req, res, next) => {
+  Person.countDocuments({}).then((result) => {
+    res.send(`<p>Total ${result} people in database </br></br> ${Date()}</p>`)
   })
 })
 
-app.post("/api/persons", (req, res) => {
+app.post("/api/persons", (req, res, next) => {
   const body = req.body
   if (!body.name || !body.number) {
     return res.status(400).json({ error: "Name and number are required" })
   }
 
-  const person = new Person({
-    name: body.name,
-    number: body.number,
-  })
+  Person.findOne({ name: body.name }).then((existingPerson) => {
+    if (existingPerson) {
+      const updatedPerson = {
+        name: body.name,
+        number: body.number,
+      }
 
-  person.save().then((savedPerson) => {
-    res.json(savedPerson)
+      Person.findByIdAndUpdate(existingPerson._id, updatedPerson, { new: true })
+        .then((updatedResult) => {
+          res.json(updatedResult)
+        })
+        .catch((error) => {
+          next(error)
+        })
+    } else {
+      const person = new Person({
+        name: body.name,
+        number: body.number,
+      })
+
+      person
+        .save()
+        .then((savedPerson) => {
+          res.json(savedPerson)
+        })
+        .catch((error) => {
+          next(error)
+        })
+    }
   })
 })
 
-app.delete("/api/persons/:id", (req, res) => {
+app.delete("/api/persons/:id", (req, res, next) => {
   const id = req.params.id
-  Person.findByIdAndDelete(id).then((deletedPerson) => {
-    res.status(204).end()
-  })
+  Person.findByIdAndDelete(id)
+    .then((deletedPerson) => {
+      res.status(204).end()
+    })
+    .catch((error) => {
+      next(error)
+    })
 })
 
-app.put("/api/persons/:id", (req, res) => {
+app.put("/api/persons/:id", (req, res, next) => {
   const id = req.params.id
   const { name, number } = req.body
 
-  updatedPerson = {
+  const updatedPerson = {
     name,
     number,
   }
 
-  Person.findByIdAndUpdate(id, updatedPerson).then((result) => {
-    res.json(result)
-  })
+  Person.findByIdAndUpdate(id, updatedPerson, { new: true })
+    .then((result) => {
+      res.json(result)
+    })
+    .catch((error) => {
+      next(error)
+    })
 })
+
+const errorHandler = (error, req, res, next) => {
+  console.log(error)
+  if (error.name === "CastError") {
+    return res.status(400).send({ error: "malformatted id" })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
--- part3/practice/index.js
+++ part3/practice/index.js
@@ -39,26 +39,31 @@ app.get("/api/notes", (request, response) => {
 //   res.json(notes)
 // })
 
-app.get("/api/notes/:id", (request, response) => {
-  Note.findById(request.params.id).then((note) => {
-    response.json(note)
-  })
+app.get("/api/notes/:id", (request, response, next) => {
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch((error) => {
+      next(error)
+    })
 })
 
-// app.delete("/api/notes/:id", (request, response) => {
-//   const id = request.params.id
-//   notes = notes.filter((note) => note.id !== id)
+app.delete("/api/notes/:id", (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end()
+    })
+    .catch((error) => {
+      next(error)
+    })
+})
 
-//   response.status(204).end()
-// })
-
-// const generateId = () => {
-//   const maxId =
-//     notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0
-//   return String(maxId + 1)
-// }
-
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
   const body = request.body
 
   if (!body.content) {
@@ -72,10 +77,42 @@ app.post("/api/notes", (request, response) => {
     important: body.important || false,
   })
 
-  note.save().then((savedNote) => {
-    response.json(savedNote)
-  })
+  note
+    .save()
+    .then((savedNote) => {
+      response.json(savedNote)
+    })
+    .catch((error) => {
+      next(error)
+    })
 })
+
+app.put("/api/notes/:id", (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then((updatedNote) => {
+      response.json(updatedNote)
+    })
+    .catch((error) => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
