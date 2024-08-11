5ea481228c14af0c2c22e2f8439d809e37416794
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-08-11 15:21:19 +0530

    finished exercise 4.2

---
+++ part4/blog-list/controllers/blogs.js
@@ -0,0 +1,30 @@
+const blogsRouter = require("express").Router()
+const Blog = require("../models/blog")
+
+blogsRouter.get("/", (req, res) => {
+  Blog.find({}).then((blogs) => {
+    res.json(blogs)
+  })
+})
+
+blogsRouter.post("/", (req, res, next) => {
+  const body = req.body
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  })
+
+  blog
+    .save()
+    .then((savedBlog) => {
+      res.json(savedBlog)
+    })
+    .catch((error) => {
+      next(error)
+    })
+})
+
+module.exports = blogsRouter
--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -1,41 +1,35 @@
-const express = require("express")
-const app = express()
-const cors = require("cors")
-const mongoose = require("mongoose")
-require("dotenv").config()
+const app = require("./app")
+const config = require("./utils/config")
+const logger = require("./utils/logger")
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
+app.listen(config.PORT, () => {
+  logger.info(`Server is running on ${config.PORT}`)
 })
 
-const Blog = mongoose.model("Blog", blogSchema)
 
-const mongoUrl = process.env.MONGO_URL
-console.log("Connect to", mongoUrl)
+// const mongoUrl = process.env.MONGO_URL
+// console.log("Connect to", mongoUrl)
 
-mongoose.connect(mongoUrl)
+// mongoose.connect(mongoUrl)
 
-app.use(cors())
-app.use(express.json())
+// app.use(cors())
+// app.use(express.json())
 
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
+// app.get("/api/blogs", (request, response) => {
+//   Blog.find({}).then((blogs) => {
+//     response.json(blogs)
+//   })
+// })
 
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body)
+// app.post("/api/blogs", (request, response) => {
+//   const blog = new Blog(request.body)
 
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
+//   blog.save().then((result) => {
+//     response.status(201).json(result)
+//   })
+// })
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`)
-})
+// const PORT = 3003
+// app.listen(PORT, () => {
+//   console.log(`Server is running on ${PORT}`)
+// })
