4b29934c16f7799fe165e63b0e135b4d1a7e9849
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-08-17 15:55:17 +0530

    exercise 4.15 done

---
+++ part4/blog-list/controllers/users.js
@@ -0,0 +1,26 @@
+const bcrypt = require("bcryptjs")
+const usersRouter = require("express").Router()
+const User = require("../models/user")
+
+usersRouter.get("/", async (req, res) => {
+  const users = await User.find({})
+  res.json(users)
+})
+
+usersRouter.post("/", async (req, res) => {
+  const { username, name, password } = req.body
+
+  const saltrounds = 10
+  const passwordHash = await bcrypt.hash(password, saltrounds)
+
+  const user = new User({
+    username,
+    name,
+    passwordHash,
+  })
+
+  const savedUser = await user.save()
+  res.status(201).json(savedUser)
+})
+
+module.exports = usersRouter
--- part4/blog-list/models/user.js
+++ part4/blog-list/models/user.js
@@ -8,12 +8,6 @@ const userSchema = new mongoose.Schema({
   },
   name: String,
   passwordHash: String,
-  notes: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Note",
-    },
-  ],
 })
 
 userSchema.set("toJSON", {
