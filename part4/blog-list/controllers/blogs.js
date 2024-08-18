5b83406171321a19d7474ce1d7523638bc8785d0
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-08-18 13:10:57 +0530

    exercise 4.20 done

--- part4/blog-list/controllers/blogs.js
+++ part4/blog-list/controllers/blogs.js
@@ -3,14 +3,6 @@ const Blog = require("../models/blog")
 const User = require("../models/user")
 const jwt = require("jsonwebtoken")
 
-const getTokenFrom = (request) => {
-  const authorization = request.get("authorization")
-  if (authorization && authorization.startsWith("Bearer ")) {
-    return authorization.replace("Bearer ", "")
-  }
-  return null
-}
-
 blogsRouter.get("/", async (req, res) => {
   const blogs = await Blog.find({}).populate("user", {
     username: 1,
@@ -22,7 +14,7 @@ blogsRouter.get("/", async (req, res) => {
 
 blogsRouter.post("/", async (req, res, next) => {
   const body = req.body
-  const decodedToken = jwt.verify(getTokenFrom(req), process.env.SECRET)
+  const decodedToken = jwt.verify(req.token, process.env.SECRET)
 
   if (!decodedToken.id) {
     return res.status(401).json({ error: "token invalid" })
--- part4/blog-list/utils/middleware.js
+++ part4/blog-list/utils/middleware.js
@@ -32,8 +32,19 @@ const errorHandler = (error, request, response, next) => {
   next(error)
 }
 
+const getTokenFrom = (request, response, next) => {
+  const authorization = request.get("authorization")
+  if (authorization && authorization.startsWith("Bearer ")) {
+    request.token = authorization.replace("Bearer ", "")
+  } else {
+    request.token = null
+  }
+  next()
+}
+
 module.exports = {
   requestLogger,
   unknownEndpoint,
   errorHandler,
+  getTokenFrom,
 }
