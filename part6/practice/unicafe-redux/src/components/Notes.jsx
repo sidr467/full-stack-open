83496498d79244e53957b6b4f69df24cc628ac1f
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-09-06 17:22:36 +0530

    practice many reducer redux

--- part6/practice/unicafe-redux/src/components/Notes.jsx
+++ part6/practice/unicafe-redux/src/components/Notes.jsx
@@ -1,31 +1,35 @@
-import { useDispatch, useSelector } from 'react-redux'
-import { toggleImportanceOf } from '../reducers/noteReducer'
+import { useDispatch, useSelector } from "react-redux"
+import { toggleImportanceOf } from "../reducers/noteReducer"
 
 const Note = ({ note, handleClick }) => {
-  return(
+  return (
     <li onClick={handleClick}>
-      {note.content} 
-      <strong> {note.important ? 'important' : ''}</strong>
+      {note.content}
+      <strong> {note.important ? "important" : ""}</strong>
     </li>
   )
 }
 
 const Notes = () => {
-
   const dispatch = useDispatch()
-  const notes = useSelector(state => state)
+  const notes = useSelector((state) => {
+    if (state.filter === "ALL") {
+      return state.notes
+    }
+    return state.filter === "IMPORTANT"
+      ? state.notes.filter((note) => note.important)
+      : state.notes.filter((note) => !note.important)
+  })
 
-  return(
+  return (
     <ul>
-      {notes.map(note =>
+      {notes.map((note) => (
         <Note
           key={note.id}
           note={note}
-          handleClick={() => 
-            dispatch(toggleImportanceOf(note.id))
-          }
+          handleClick={() => dispatch(toggleImportanceOf(note.id))}
         />
-      )}
+      ))}
     </ul>
   )
 }
--- part6/practice/unicafe-redux/src/main.jsx
+++ part6/practice/unicafe-redux/src/main.jsx
@@ -1,15 +1,33 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import { createStore } from "redux"
 import { Provider } from "react-redux"
-
+import { createStore, combineReducers } from "redux"
+import { createNote } from './reducers/noteReducer'
+import { filterChange } from './reducers/filterReducer'
 import noteReducer from "./reducers/noteReducer"
 import App from "./App"
+import filterReducer from "./reducers/filterReducer"
 
-const store = createStore(noteReducer)
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer,
+})
+
+const store = createStore(reducer)
+// store.subscribe(() => console.log(store.getState()))
+// store.dispatch(filterChange("IMPORTANT"))
+// store.dispatch(
+//   createNote("combineReducers forms one reducer from many simple reducers")
+// )
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <App />
   </Provider>
 )
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <Provider store={store}>
+//     <div />
+//   </Provider>
+// )
