16b0415e604c24aacb636fc3e649b12dc1fc0902
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-10-07 21:05:10 +0530

    practice

--- part8/practice/apollo-client/src/App.jsx
+++ part8/practice/apollo-client/src/App.jsx
@@ -1,5 +1,28 @@
+import { gql, useQuery } from "@apollo/client"
+import Persons from "./components/Persons"
+
+const ALL_PERSON = gql`
+  query {
+    allPersons {
+      name
+      phone
+      id
+    }
+  }
+`
+
 function App() {
-  return <></>
+  const result = useQuery(ALL_PERSON)
+
+  if (result.loading) {
+    return <div>Loading.....</div>
+  }
+
+  return (
+    <div>
+      <Persons persons={result.data.allPersons} />
+    </div>
+  )
 }
 
 export default App
