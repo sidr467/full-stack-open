6efece2dff3ae9bc518a5a464c62824456d25f3f
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-10-08 17:11:54 +0530

    exercises 8.8 and 8.9 done

---
+++ part8/library-frontend/src/components/Books.jsx
@@ -0,0 +1,52 @@
+import { gql, useQuery } from "@apollo/client"
+
+const ALL_BOOKS = gql`
+  query {
+    allBooks {
+      title
+      published
+      author
+      id
+      genres
+    }
+  }
+`
+
+const Books = (props) => {
+  const result = useQuery(ALL_BOOKS)
+
+  if (result.loading) {
+    return <div>Loading...</div>
+  }
+
+  if (!props.show) {
+    return null
+  }
+
+  const books = result.data.allBooks
+
+  return (
+    <div>
+      <h2>books</h2>
+
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>author</th>
+            <th>published</th>
+          </tr>
+          {books.map((a) => (
+            <tr key={a.title}>
+              <td>{a.title}</td>
+              <td>{a.author}</td>
+              <td>{a.published}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Books
