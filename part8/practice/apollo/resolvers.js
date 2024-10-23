c6eb75fdbe583b486380aa7fdfea84cf5b5c22ae
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-10-23 17:25:15 +0530

    Update resolvers.js

--- part8/practice/apollo/resolvers.js
+++ part8/practice/apollo/resolvers.js
@@ -13,7 +13,9 @@ const resolvers = {
       if (!args.phone) {
         return Person.find({}).populate("friendOf")
       }
-      return Person.find({ phone: { $exists: args.phone === "YES" } }).populate('friendOf')
+      return Person.find({ phone: { $exists: args.phone === "YES" } }).populate(
+        "friendOf"
+      )
     },
     findPerson: async (root, args) => Person.findOne({ name: args.name }),
     me: (root, args, context) => {
@@ -27,11 +29,11 @@ const resolvers = {
         city,
       }
     },
-    friendOf: async (root) => {
-      const friends = await User.find({ friends: { $in: [root._id] } })
-      console.log("User.find")
-      return friends
-    },
+    // friendOf: async (root) => {
+    //   const friends = await User.find({ friends: { $in: [root._id] } })
+    //   console.log("User.find")
+    //   return friends
+    // },
   },
   Mutation: {
     addPerson: async (root, args, context) => {
